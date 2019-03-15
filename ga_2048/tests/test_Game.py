b04fb23f69ebc96b3c8abeeccad29d0d0bf23453
Kshitij68 <k.mathur68@gmail.com>
2019-03-15 21:44:09 +0530

    Add test case

---
+++ ga_2048/tests/test_Game.py
@@ -0,0 +1,23 @@
+from ga_2048.my_2048 import Game
+import unittest
+import numpy as np
+
+
+class TestGame(unittest.TestCase):
+
+    # def setUpClass(cls):
+    #     pass
+    #
+    # def setUp(self):
+    #     pass
+
+    def test_check_game_status(self):
+        game = Game()
+        game.tiles = np.array([
+            [8, 16, 64, 8],
+            [2, 4, 16, 64],
+            [4, 2, 16, 64],
+            [2, 2, 8, 2]
+        ])
+        self.assertEqual(game.check_game_status(),True)
+
