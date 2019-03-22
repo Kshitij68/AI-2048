f1ca1bf863fd9f4ef5cdc0587800413f09474f97
Kshitij68 <k.mathur68@gmail.com>
2019-03-22 16:29:29 +0530

    Update bot creating process, fix bugs

--- ga_2048/manual.py
+++ ga_2048/manual.py
@@ -1,7 +1,8 @@
 from ga_2048.utils import get_logger
 from ga_2048.CNNBot import CNNBot
 from ga_2048.NNBot import NNBot
-from ga_2048.my_2048 import Game
+from ga_2048.simulator import Game
+
 
 # TODO: Add Asserts and test cases
 # TODO: The CNN has to become much faster to train fast
@@ -9,31 +10,49 @@ from ga_2048.my_2048 import Game
 
 class Optimizer:
 
-    def __init__(self,n_generations, n_children,convolutions=1,network='CNN'):
-        if network == 'NN':
-            self.model = NNBot
-        else:
-            self.model = CNNBot
+    def __init__(self, n_generations, n_children, convolutions=1, network='CNN',
+                 disable_game_logs= True, disable_optimizer_logs = True, disable_model_logs =True):
+
+        self.disable_game_logs = disable_game_logs
+        self.disable_optimizer_logs = disable_optimizer_logs
+        self.disable_model_logs = disable_model_logs
+        self.network = network
         self.convolutions = convolutions
+
         if n_children < 10:
             raise ValueError("Please provide number of children to be atleast 10")
         self.n_children = n_children
+
         self.logger = get_logger("Optimizer")
+        self.logger.disabled = self.disable_optimizer_logs
+
         self.bots = list()
         for i in range(self.n_children):
-            bot = self.model('Bot Number' + str(i + 1))
+            bot = self.create_bot(name='bot number' + str(i))
             self.bots.append(bot)
+
         self.high_score = 0
+
         self.n_generations = n_generations
+
         self.survival_rate = 0.2
-        self.new_children = int((self.n_children - (self.survival_rate*self.n_children))/(self.survival_rate*self.n_children))
+        self.new_children = int(
+            (self.n_children - (self.survival_rate * self.n_children)) / (self.survival_rate * self.n_children))
         self.logger.info("Will be creating {} children per bot".format(self.new_children))
+
+    def create_bot(self,name,parameters = None):
+        if self.network == 'NN':
+            return NNBot(name=name, disable_logs=self.disable_model_logs,parameters=parameters)
+        elif self.network == 'CNN':
+            return CNNBot(name=name, disable_logs=self.disable_model_logs,parameters=parameters)
+        else:
+            raise ValueError("The current model only supports NN and CNN. Please pass 'NN' for Neural Network "
+                             "architecture and 'CNN' for Convolution Neural Network based architecture ")
 
     def get_high_scores(self):
         bot_scores = list()
-        disable_logs = True
-        for index,child in enumerate(self.bots):
-            game = Game(show = False, disable_logs=disable_logs)
+        for index, child in enumerate(self.bots):
+            game = Game(show=False, disable_logs=self.disable_game_logs)
             while True:
                 prediction = self.bots[index].predict(game.tiles.transpose())
                 output = game.run(prediction[0])
@@ -57,30 +76,28 @@ class Optimizer:
                                 break
 
                     else:
-                        bot_scores.append([index,game.total_points,output[1]])
+                        bot_scores.append([index, game.total_points, output[1]])
                         break
-        bot_scores.sort(key=lambda x: x[1],reverse=True)
+        bot_scores.sort(key=lambda x: x[1], reverse=True)
         return bot_scores
 
     def main(self):
         for gen in range(self.n_generations):
-            self.logger.info('Running generation number {} with total {} bots'.format(gen,len(self.bots)))
-            scores = self.get_high_scores()[:int(self.survival_rate*self.n_children)]
+            self.logger.info('Running generation number {} with total {} bots'.format(gen, len(self.bots)))
+            scores = self.get_high_scores()[:int(self.survival_rate * self.n_children)]
             survived_bots_indexes = set([value[0] for value in scores])
             self.logger.info("The highest score obtained is: {}".format(scores[0][1]))
             new_bots = list()
             child_index = 0
             self.logger.info("Scores are {}".format(scores))
-            for index,bot in enumerate(self.bots):
+            for index, bot in enumerate(self.bots):
                 if index in survived_bots_indexes:
                     new_bots.append(bot)
-                    if self.convolutions == 1:
-                        bot_matrix = bot.matrix[0]
-                    else:
-                        raise NotImplementedError()
-                        # TODO: This method is jugaddoo. Get the elements and then remake the numpy matrix to avoid pass by reference
+                    bot_matrix = bot.matrix.copy(deep=True)
+                    # TODO: Check working for NN and CNN
                     for _ in range(self.new_children):
-                        new_bot = self.model(name='Bot Number ' + str(child_index + 1),matrix=[bot_matrix.copy()])
+                        new_bot = self.create_bot(name='bot number ' + str(child_index+1),
+                                                  parameters=bot_matrix.copy(deep=True))
                         new_bot.mutate()
                         new_bots.append(new_bot)
                         child_index += 1
@@ -88,6 +105,6 @@ class Optimizer:
         return None
 
 
-if __name__== "__main__":
-    optimize = Optimizer(n_generations=1000,n_children=10,convolutions=1,network='CNN')
+if __name__ == "__main__":
+    optimize = Optimizer(n_generations=1000, n_children=10, convolutions=1, network='CNN',disable_game_logs= True, disable_optimizer_logs = True, disable_model_logs =True)
     optimize.main()
